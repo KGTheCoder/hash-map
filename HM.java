2ddba2b10ca6d755a7fff76bae3e30aaeb1170c8
Kwasi G <40185144+TheOneWhoSits@users.noreply.github.com>
2020-01-04 15:52:07 -0800

    Add files via upload

---
+++ HM.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HM {
+   public static void main(String[] args) {
+      HashMap<String, Integer> map = new  HashMap<>();
+      
+      print(map);
+      map.put("cat", 2);
+      map.put("dog", 3);
+      map.put("crow", 4);
+      
+      System.out.println("Map size: " + map.size());
+      
+      print(map);
+   }
+   
+   public static void print(Map<String, Integer> map) {
+      if (map.isEmpty()) {
+         System.out.println("map is empty");
+      } else {
+         System.out.println(map);
+      }
+   }
+}
+
